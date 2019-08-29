c1452f37d52e2f572e928a1a6ce49a54a0c68140
Jai Shukla <jay@live.co.in>
2019-08-30 00:57:11 +0530

    new simulation code

---
+++ simulations.py
@@ -0,0 +1,88 @@
+import numpy as np 
+
+
+# Science 
+
+def NusseltNumber(ReynodNumber,PrandtlNumber,L,D):
+    return 1.86*(ReynodNumber*PrandtlNumber/(L/D))**1.5 
+
+def SheerwoodNumber(ReynodNumber,ShmidtNumber):
+    return 0.023*ReynodNumber**0.83*ShmidtNumber**0.33
+
+def h_heat(length_channel,equivalent_diameter,themal_conductivity,ReynodNumber,PrandtlNumber):
+    return NusseltNumber(ReynodNumber,PrandtlNumber,length_channel,equivalent_diameter)*themal_conductivity/length_channel
+
+def h_mass(ReynodNumber,ShmidtNumber,length_channel,themal_conductivity):
+    return SheerwoodNumber(ReynodNumber,ShmidtNumber)*themal_conductivity/length_channel
+
+
+
+# Initial Condition
+
+temp_inlet_dry = 
+absHumidity_inlet_dry = 
+temp_productAir = 
+temp_water = 
+
+
+# Geometrical Parameters 
+
+height = 
+length = 
+width = 
+
+# Simulation Parameters
+
+steps = 
+convergence = 
+
+# Looping function to simulate the value
+
+'''
+    Mass flow rate : m 
+    Specific Heat Capacity : c
+    Absoulte Humidity : w
+    enthalpy of vaporisation : iv
+
+
+    Assumptions :
+    1. Temperature of water is maintained using a thermocouple implies (temperature of water remains same)
+    2. 
+'''
+
+
+def update_temperature_product():
+    return ( h_heat*dx*b*(T_productAir - T_water) + m_productAir*c_productAir*T_productAir )/ ( m_productAir*c_productAir ) 
+
+def update_temperature_workingAir_dry():
+    return ( h_heat*dx*b*(T_workingAir_dry - T_water) + m_workingAir_dry*c_productAir*T_workingAir_dry  ) / ( m_workingAir_dry*c_productAir )
+
+def update_absoluteHumidity_workingAir_wet():
+    return ( h_mass*dx*b*(w_saturated_wet - w_workingAir_wet) + m_workingAir_wet*w_workingAir_wet  ) / ( m_workingAir_wet )
+
+def update_temperature_workingAir_wet():
+    return ( h_heat*dx*b*(T_water-T_workingAir_wet) + h_mass*(dx)*b*iv*(w_saturated_wet - w_workingAir_wet) + T_workingAir_wet*m_workingAir_dry*c_productAir) / (m_workingAir_dry*c_productAir)
+
+def update_massFlowRate_workingAir_wet():
+    dT = 
+    return -1. * (m_productAir*c_productAir*dT + m_workingAir_wet*iv*(w_saturated_wet - w_workingAir_wet + c_water*m_water*T_water)) / (c_water*T_water)
+
+def waitForConvergence(convergence_factor,function,*argvs):
+    values = 
+    while(convergence_factor >= waitForConvergence(convergence_factor,function,))
+
+# Simulations 
+
+# TODO: make a matrix to solve the equation and the use the internal loop for calculating the value of the values
+
+dx = length_channel/steps 
+T_productAir_simulations = []
+T_workingAir_dry_simulations = []
+
+for n in range(steps):
+
+    update_temperature_product()
+    
+
+
+
