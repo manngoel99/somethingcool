efe070d7c6480b3e59bba53d9a31d17bdc0d251e
Jai Shukla <jay@live.co.in>
2019-08-31 16:55:44 +0530

    Room Cooling Temperature done

--- simulations.py
+++ simulations.py
@@ -149,8 +149,10 @@ def waitForConvergence(convergence_factor,function,seed,*argv):
 # TODO: make a matrix to solve the equation and the use the internal loop for calculating the value of the values
 
 
-def simulate(steps,convergence_factor,*initalCondition):
+def simulate(steps,convergence_factor,initalCondition):
 
+    initalCondition = initalCondition[0]
+    
     length_channel = initalCondition[6]
     dx = length_channel/steps 
     T_productAir = [initalCondition[0]]
@@ -201,11 +203,96 @@ def simulate(steps,convergence_factor,*initalCondition):
         T_water.append(T_water_update)
 
 
-    return [T_productAir,T_workingAir_dry,T_workingAir_wet,w_workingAir_wet,T_water]
+    return T_productAir,T_workingAir_dry,T_workingAir_wet,w_workingAir_wet,T_water,m_productAir 
 
 
 #Check the parameters again as few of them are added to check wheather calculation are working or not
 print (simulate(10,1,308,308,308,0.024,293,0.026,0.5,0.05,0.027,2.26e6 ,0.03,91.6e-5,1007,12,10))
 
-def simulateTimeBasedCooling():
-   print("Hello World") 
+def simulateTimeBasedCooling(number_channel,timeDivision,duration_inMin,volumn_room,water_temp,temp_room,humidity_room,initalCondition):
+   
+    # Write the variable about the area of cross section of channel that will be used to release air into the room
+    global area 
+    global velocity_air_intake
+
+    timeDuration = 1/timeDivision
+    indexes = range(timeDivision*60*duration_inMin)
+    total_volume_exchange = area*number_channel*velocity_air_intake*timeDuration 
+
+    # Constants
+    '''
+    length_channel = initalCondition[6]
+    dx = length_channel/steps 
+    T_productAir = initalCondition[0]
+    T_workingAir_dry = initalCondition[1]
+    T_workingAir_wet = initalCondition[2]
+    w_workingAir_wet = initalCondition[3]
+    T_water = initalCondition[4]
+    h_heat = initalCondition[5]
+    
+    b = initalCondition[7]
+    #T_water = [initalCondition[8]]
+    h_mass = initalCondition[8]
+    iv = initalCondition[9]
+    w_saturated_wet = initalCondition[10]
+    #w_workingAir_wet = [initalCondition[12]]
+    #T_workingAir_wet = [initalCondition[13]]
+    m_workingAir_dry = initalCondition[11]
+    c_productAir = initalCondition[12]
+    m_productAir = initalCondition[13]
+    c_water = initalCondition[14]
+    '''
+
+    # variable to store the output
+    T_productAir_allTime = []
+    T_workingAir_dry_allTime = []
+    T_workingAir_wet_allTime = []
+    w_workingAir_wet_allTime = []
+    T_water_allTime = []
+    
+
+    for i in indexes:
+        #Simulations Variables
+        T_productAir_temp,T_workingAir_dry_temp,T_workingAir_wet_temp,w_workingAir_wet_temp,T_water_temp,m_flowRate =     simulate(100,0.01,initalCondition) 
+
+        # saveVariables form the simulation
+        T_productAir_allTime.append(T_productAir_temp)
+        T_workingAir_dry_allTime.append(T_workingAir_dry)
+        T_workingAir_wet_allTime.append(T_workingAir_wet_temp)
+        w_workingAir_wet_allTime.append(w_workingAir_wet_temp)
+        T_water_allTime.append(T_water_temp)
+
+    
+        #modifing the inital condition for next step of simulation
+        initalCondition[0]= cool_room(m_flowRate,(n+1)*timeDuration,volumn_room,T_productAir[-1],temp_room)
+
+
+    np.savetxt('output/T_productAir',np.array(T_productAir_allTime))
+
+    np.savetxt('output/T_workingAir_dry',np.array(T_workingAir_dry_allTime))
+    np.savetxt('output/T_workingAir_wet',np.array(T_workingAir_wet_allTime))
+    np.savetxt('output/w_workingAir_wet',np.array(w_workingAir_wet_allTime))
+    np.savetxt('output/T_water',np.array(T_water_allTime))
+
+
+    print("Simulation executed successfully")
+
+# Make global variable given below in the function
+
+
+def cool_room(m_flowrate,duration,volumn_room,temp_airOut, temp_room):
+    global heat_capacity_air
+    global density
+    
+    Final_temp = ( volumn_room*density*heat_capacity_air*temp_room +m_flowrate*duration*heat_capacity_air*temp_airOut ) / (volumn_room*density*heat_capacity_air + m_flowrate*duration*heat_capacity_air)
+
+        
+
+
+
+
+        
+
+
+
+
